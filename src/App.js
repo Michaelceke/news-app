8eb9fa3d0266033a946fe4d1dd67404e01107e22
Michaelceke <ekemicheal@yahoo.com>
2021-09-25 23:25:56 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+let x;
 
 function App() {
+  console.log("hello");
   return (
     <div className="App">
       <header className="App-header">
